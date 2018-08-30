4fb49c4e414105bca21aa56ad4bce690bd9d050c
Nick <rezoh.c@gmail.com>
2018-08-30 14:40:30 +0300

    refactor: remove unused constant

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,1 @@
-export const errorMessages = {
-  geolocation: {
-    title: 'Geolocation Permissions',
-    message: 'We need permissions to your location to provide weather information. Please, enable it in Settings.'
-  }
-}
-
 export const forecastAgeTimeout = 5000
